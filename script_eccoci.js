90ed5993aaf301fdbd7627366d8f6462137f7283
Rappresentanti <r2054511@gmail.com>
2024-06-11 13:32:41 +0200

    Update script_eccoci.js

--- script_eccoci.js
+++ script_eccoci.js
@@ -1,16 +1,15 @@
-document.getElementById('newsletter-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  var email = document.getElementById('email').value;
-  // Aggiungi qui la logica per inviare l'email al tuo server di newsletter
-  alert('Grazie per esserti iscritto alla nostra newsletter, ' + email + '!');
-});
-// Funzione per ridimensionare il banner e il titolo al scroll
-window.onscroll = function() {
-  var header = document.getElementById('header');
-  var banner = document.getElementById('banner');
-  if (window.pageYOffset > 100) {
-    banner.classList.add('smaller');
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  var logo = document.querySelector('.logo');
+  var socialIcons = document.querySelector('.social-icons');
+  
+  if (window.scrollY > 100) {
+    header.classList.add('small-header');
+    logo.classList.add('small-logo');
+    socialIcons.classList.add('social-icons-right');
   } else {
-    banner.classList.remove('smaller');
+    header.classList.remove('small-header');
+    logo.classList.remove('small-logo');
+    socialIcons.classList.remove('social-icons-right');
   }
-};
+});
