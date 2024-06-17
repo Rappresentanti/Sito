7de0c3b97bade0f431011e2eccac4ba34ed96250
Rappresentanti <r2054511@gmail.com>
2024-06-17 21:16:32 +0000

    roba a cazzus

--- script_main.js
+++ script_main.js
@@ -26,10 +26,6 @@ var x = setInterval(function() {
     document.getElementById("countdown").innerHTML = "EXPIRED";
   }
 }, 1000);
-function toggleMenu() {
-  var sideMenu = document.getElementById('sideMenu');
-  sideMenu.classList.toggle('active');
-}
 document.getElementById('logo').addEventListener('click', function() {
   var menu = document.getElementById('menu');
   var logo = this;
